5036787a01e42718914d5b2f78a6918bbc430a26
andy daniel pujol <apujo010@AD.CS.FIU.EDU>
2018-11-28 18:04:22 -0500

    Added Banner Video to Home Page but it does not show yet, have to fix it.

--- src/Home/home.js
+++ src/Home/home.js
@@ -6,7 +6,15 @@ class Home extends React.Component{
     render(){
         return(
             <div>
+            <div className = "video">
+            <video id="fullscreen-video" autoplay poster="../assets/videos/background.jpg" loop>
+            <source src="../assets/videos/background.mp4" type="video/mp4"/>
+            <source src="../assets/videos/background.m4v"/>
+            <source src="../assets/videos/background.webm" type="video/webm"/>
+            <source src="../assets/videos/background.ogv" type="video/ogv"/>
+            </video>
             <div className = "logo"></div>
+            </div>
             <div>
             <b>NUI</b> Lab <br />
             <a href="http://www.cs.colostate.edu/cstop/">CSU's</a> Natural User Interaction Lab
