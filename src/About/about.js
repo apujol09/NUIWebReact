4a356db18cbaf527968e875cd2001ff9ea691738
andy daniel pujol <apujo010@AD.CS.FIU.EDU>
2019-01-15 16:48:17 -0500

    About Us page content is formatted nicely now. I just need to fill in the content from the database.

--- src/About/about.js
+++ src/About/about.js
@@ -31,23 +31,25 @@ class About extends React.Component{
             <div>
                 <div className="header">
                     <button className="toggleButton1" onClick={this.toggleSecondImage}>&larr;</button>
-                        { this.state.toggleSecondImage ? (
-                            <img className="bigHeader" src={header1} />
-                            ):
-                            (
-                            <img className="bigHeader" src={header2} />
-                            )
-                        }
+                            { this.state.toggleSecondImage ? (
+                                <img className="bigHeader" src={header1} />
+                                ):
+                                (
+                                <img className="bigHeader" src={header2} />
+                                )
+                            }
                     <button className="toggleButton2" onClick={this.toggleSecondImage}>&rarr;</button>
                 </div>
                 
-                <br />
-                <img className="map-image" src={map} />
-                <article className="lab-text">
-                    <h4>Who are We?</h4>
-                    <p>We are a multi-disciplinary group, with our primary site at Colorado State University and with a still active site at Florida International University. We have included a diverse background of knowledge and origins. While CS-centric, former students have included majors, such as Statistics, Mathematics, Biology, Psychology, Engineering, and Architecture, among others. We have had members from multiple places, including USA, Cuba, Chile, Colombia, Jamaica, Dominican Republic, China, Brazil, and Venezuela, among others.</p>
-                </article>
-                <hr />
+                <div className="lab-description">
+                    <br />
+                    <img className="map-image" src={map} />
+                    <article className="lab-text">
+                        <h4>Who are We?</h4>
+                        <p>We are a multi-disciplinary group, with our primary site at Colorado State University and with a still active site at Florida International University. We have included a diverse background of knowledge and origins. While CS-centric, former students have included majors, such as Statistics, Mathematics, Biology, Psychology, Engineering, and Architecture, among others. We have had members from multiple places, including USA, Cuba, Chile, Colombia, Jamaica, Dominican Republic, China, Brazil, and Venezuela, among others.</p>
+                    </article>
+                </div>
+                <hr className="line" />
                 <br />
                 <br />
             </div>
