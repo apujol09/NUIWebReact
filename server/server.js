b3bee75b40ea1bb2b3e469c9aff9840b316d799b
andy daniel pujol <apujo010@AD.CS.FIU.EDU>
2019-02-01 21:07:23 -0500

    Fixed a little issue with the server.

--- server/server.js
+++ server/server.js
@@ -12,7 +12,6 @@ mongoose.connect(process.env.DATABASE)
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(express.static(''));
 
 // Models
 const { Members } = require('./models/members');
