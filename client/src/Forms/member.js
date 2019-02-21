fa3fbcc3afc62ec667dbe872f3037b02e001dc55
Andy Pujol <apujo010@fiu.edu>
2019-02-21 06:21:52 -0500

    Updating a member now works as intended and changing categories moves the member from one category to another.

--- client/src/Forms/member.js
+++ client/src/Forms/member.js
@@ -19,7 +19,8 @@ class MemberForm extends Component {
             memberEducation: this.props.member ? this.props.member.education : null,
             memberWebsite: this.props.member ? this.props.member.website : null,
             memberDescription: this.props.member ? this.props.member.description : null,
-            memberCategory: this.props.member ? this.props.member.category : "facultyCSU",
+            memberCategory: this.props.category ? this.props.category : "facultyCSU",
+            categoryBackup: this.props.category ? this.props.category : null,
             selectedImage: null
         }
     }
@@ -107,30 +108,64 @@ class MemberForm extends Component {
 
     handleUpdate = event =>{
         event.persist();
-
-        axios.post(`/api/${this.state.memberCategory}/${this.props.member._id}`, { 
-            name: this.state.memberName,
-            education: this.state.memberEducation,
-            major: this.state.memberMajor,
-            position: this.state.memberPosition,
-            email: this.state.memberEmail,
-            university: this.state.memberUniversity,
-            website: this.state.memberWebsite,
-            image: this.props.member.image,
-            description: this.state.memberDescription 
-            })
-            .then(res =>{
+        if(this.props.category){
+            axios.delete(`/api/${this.state.categoryBackup}/${this.props.member._id}`).then(res => {
                 if(res.data.success === true){
                     window.location.reload();
                 }
                 else{
-                    console.log("Updating Member Failed!");
-                    console.log(res);
+                    console.log("Delete Failed!")
+                    console.log(res)
                 }
-        });
+            });
+
+            axios.post(`/api/${this.state.memberCategory}`, { 
+                name: this.state.memberName,
+                education: this.state.memberEducation,
+                major: this.state.memberMajor,
+                position: this.state.memberPosition,
+                email: this.state.memberEmail,
+                university: this.state.memberUniversity,
+                website: this.state.memberWebsite,
+                image: this.props.member.image,
+                description: this.state.memberDescription 
+                })
+                .then(res =>{
+                    if(res.data.success === true){
+                        console.log("Updating Member Success!")
+                    }
+                    else{
+                        console.log("Updating Member Failed!");
+                        console.log(res);
+                    }
+            });
+        }
+        else{
+            axios.put(`/api/${this.state.memberCategory}/${this.props.member._id}`, { 
+                name: this.state.memberName,
+                education: this.state.memberEducation,
+                major: this.state.memberMajor,
+                position: this.state.memberPosition,
+                email: this.state.memberEmail,
+                university: this.state.memberUniversity,
+                website: this.state.memberWebsite,
+                image: this.props.member.image,
+                description: this.state.memberDescription 
+                })
+                .then(res =>{
+                    if(res.data.success === true){
+                        window.location.reload();
+                    }
+                    else{
+                        console.log("Updating Member Failed!");
+                        console.log(res);
+                    }
+            });
+        }
     }
 
     render() {
+
         return (
             <div>
                 <Container>
@@ -147,22 +182,22 @@ class MemberForm extends Component {
                                 <AvForm onSubmit={this.props.member ? this.handleUpdate : this.handleSubmit}>
                                     <AvGroup>
                                         <Label className="form-label" for="example">Name *</Label>
-                                        <AvInput name="name" id="memberName" value={this.state.nameValue} placeholder="Enter Member Name Here" required onChange={this.nameChange}/>
+                                        <AvInput name="name" id="memberName" value={this.state.memberName} placeholder="Enter Member Name Here" required onChange={this.nameChange}/>
                                         <AvFeedback className="av-feedback">This Field is Required!</AvFeedback>
                                     </AvGroup>
                                     <AvGroup>
                                         <Label className="form-label" for="memberEmail">Email *</Label>
-                                        <AvInput name="email" id="memberEmail" value={this.state.emailValue} placeholder="Enter Member Email here" required onChange={this.emailChange}/>
+                                        <AvInput name="email" id="memberEmail" value={this.state.memberEmail} placeholder="Enter Member Email here" required onChange={this.emailChange}/>
                                         <AvFeedback className="av-feedback">This Field is Required!</AvFeedback>
                                     </AvGroup>
                                     <AvGroup>
                                         <Label className="form-label" for="memberPosition">Position *</Label>
-                                        <AvInput name="position" id="memberPosition" value={this.state.positionValue} placeholder="Enter Member Position here" required onChange={this.positionChange}/>
+                                        <AvInput name="position" id="memberPosition" value={this.state.memberPosition} placeholder="Enter Member Position here" required onChange={this.positionChange}/>
                                         <AvFeedback className="av-feedback">This Field is Required!</AvFeedback>
                                     </AvGroup>
                                     <AvGroup>
                                         <Label className="form-label" for="memberUniversity">University *</Label>
-                                        <AvInput name="university" id="memberUniversity" value={this.state.universityValue} placeholder="Enter Member University here" required onChange={this.universityChange}/>
+                                        <AvInput name="university" id="memberUniversity" value={this.state.memberUniversity} placeholder="Enter Member University here" required onChange={this.universityChange}/>
                                         <AvFeedback className="av-feedback">This Field is Required!</AvFeedback>
                                         <FormText className="form-text" color="muted">
                                             Please refer to About Us page to see the format of the University Field. Keep data 
@@ -171,12 +206,12 @@ class MemberForm extends Component {
                                     </AvGroup>
                                     <AvGroup>
                                         <Label className="form-label" for="memberMajor">Major *</Label>
-                                        <AvInput name="major" type="text" id="memberMajor" value={this.state.majorValue} placeholder="Enter Member Major here" required onChange={this.majorChange}/>
+                                        <AvInput name="major" type="text" id="memberMajor" value={this.state.memberMajor} placeholder="Enter Member Major here" required onChange={this.majorChange}/>
                                         <AvFeedback className="av-feedback">This Field is Required!</AvFeedback>
                                     </AvGroup>
                                     <AvGroup>
                                         <Label className="form-label" for="memberEducation">Education Level *</Label>
-                                        <AvInput name="education" type="text" id="memberEducation" value={this.state.educationValue} placeholder="Enter Member Education Level here" required onChange={this.educationChange}/>
+                                        <AvInput name="education" type="text" id="memberEducation" value={this.state.memberEducation} placeholder="Enter Member Education Level here" required onChange={this.educationChange}/>
                                         <AvFeedback className="av-feedback">This Field is Required!</AvFeedback>
                                         <FormText className="form-text" color="muted">
                                             Please refer to About Us page to see the format of the Education Field. It sould be
@@ -185,17 +220,17 @@ class MemberForm extends Component {
                                     </AvGroup>
                                     <FormGroup>
                                         <Label className="form-label" for="memberWebsite">Website</Label>
-                                        <Input type="text" id="memberWebsite" value={this.state.websiteValue} placeholder="Enter Member Website here" onChange={this.websiteChange}/>
+                                        <Input type="text" id="memberWebsite" value={this.state.memberWebsite} placeholder="Enter Member Website here" onChange={this.websiteChange}/>
                                         <FormText className="form-text" color="muted">
                                             Only enter a URL in this field. This is not required for all members.
                                         </FormText>
                                     </FormGroup>
                                     <AvGroup>
                                         <Label className="form-label" for="memberDescription">Description *</Label>
-                                        <AvInput style={{height: "200px"}} type="textarea" value={this.state.descriptionValue} name="description" placeholder="Enter Member Description here" required onChange={this.descriptionChange}/>
+                                        <AvInput style={{height: "200px"}} type="textarea" value={this.state.memberDescription} name="description" placeholder="Enter Member Description here" required onChange={this.descriptionChange}/>
                                         <AvFeedback className="av-feedback">This Field is Required!</AvFeedback>
                                     </AvGroup>
-                                    <AvField type="select" name="select" label="Option" helpMessage="Please Select one of these categories to put the new member in!" required onChange={this.categoryChange}>
+                                    <AvField type="select" name="select" label="Option" value={this.state.memberCategory} helpMessage="Please Select one of these categories to put the new member in!" required onChange={this.categoryChange}>
                                         <option value="facultyCSU">Faculty from Colorado State University</option>
                                         <option value="facultyFIU">Faculty from Florida International University</option>
                                         <option value="members">Current Member</option>
